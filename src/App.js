ead71f0524b03207a2c8326fed95a2733bb49eac
HT43 <arsentiy.r@mail.ru>
2019-12-12 11:25:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+} from "react-router-dom";
+import './app.css';
+import Header from "./components/header/header.js"
+import Footer from "./components/footer/footer.js"
+import Sidebar from "./components/sidebar/sidebar.js";
+import Profile from "./components/profile/profile.js";
+import Dialog from "./components/dialogs/dialog";
+import Music from "./components/music/music";
+import News from "./components/news/news";
+import Settings from "./components/setting/settings";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = (props) => {
+
+    return (
+        <Router>
+            <div className="full_page">
+                <Header/>
+
+                <div className="content_cont">
+                    <Sidebar sidebarData={props.appState.sidebarData}/>
+                    <Route path='/profile'  render={ () => <Profile profileState={props.appState.profilePage}/> }/>
+                    <Route path='/dialog' render={ () => <Dialog dialogState={props.appState.dialogPage} messagesData={props.appState.messagesData}/> }/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
+                </div>
+
+                <Footer/>
+            </div>
+        </Router>
+    );
+};
+
 
 export default App;
